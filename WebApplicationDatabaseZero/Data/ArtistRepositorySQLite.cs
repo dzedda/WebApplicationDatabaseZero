db1b5db911d61c3474d18023b88fae79f6b21753
Davide Zedda <dzedda@hotmail.com>
2022-04-01 11:02:59 +0200

    Aggiungere i file di progetto.

---
+++ WebApplicationDatabaseZero/Data/ArtistRepositorySQLite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationDatabaseZero.Models;
+
+namespace WebApplicationDatabaseZero.Data
+{
+    public class ArtistRepositorySQLite : IArtistRepository
+    {
+        public List<Artist> GetArtists()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
